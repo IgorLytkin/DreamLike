a8137db75d7fb4f010d9709e8696704d4cb65da8
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-01-31 14:24:55 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# https://huggingface.co/dreamlike-art/dreamlike-photoreal-2.0
+
+from diffusers import StableDiffusionPipeline
+import torch
+import transformers
+import accelerate
+
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "photo, a church in the middle of a field of crops, bright cinematic lighting, gopro, fisheye lens"
+image = pipe(prompt).images[0]
+
+image.save("./result.jpg")

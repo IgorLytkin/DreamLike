5b0646025e1b6906e4b91a4a7c7319a804a94683
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-02-01 22:39:05 +0700

    Добавил в math.py пример с матрицами

--- math.py
+++ math.py
@@ -1,3 +1,10 @@
 import numpy as np
 vector_1 = np.array([1, 2, 3])
 print(vector_1)
+
+# Перемножение матриц
+matrix_1 = np. array ([[1, 2, 3], [3, 4, 5], [6, 7, 8]])
+matrix_2 = np. array([[16, 22, 63], [31, 54, 51], [69, 70, 87]])
+print(np. dot (matrix_1, matrix_2))
+
+print(set(["1", "ml", "ml", "1", 2, 3, "ml"]))
